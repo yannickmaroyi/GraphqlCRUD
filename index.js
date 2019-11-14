4f9fbbc6656cfab131714c75193c3f147ea8c8be
yannickmaroyi <yannickmaroyi@gmail.com>
2019-11-14 15:47:51 +0300

    database mutations added and new test

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const {
 //     hello: String
 //   }
 // `);
+
 var schema = buildSchema(`
   type User {
     id: String
@@ -29,45 +30,56 @@ var schema = buildSchema(`
   }
   type Query {
     getUsers: [User],
-    getUserInfo(id: Int) : User
+    getUserInfo(id: Int) : User,
+    hello:String
+  }
+  type Mutation {
+    updateUserInfo(id: Int, name: String, email: String, job_title: String): Boolean
+    createUser(name: String, email: String, job_title: String): Boolean
+    deleteUser(id: Int): Boolean
   }
 `);
 
 const queryDB = (req, sql, args) => new Promise((resolve, reject) => {
     req.mysqlDb.query(sql, args, (err, rows) => {
+        
         if (err)
             return reject(err);
+
         rows.changedRows || rows.affectedRows || rows.insertId ? resolve(true) : resolve(rows);
     });
 });
 
 var root = {
   getUsers: (args, req) => queryDB(req, "select * from users").then(data => data),
-  getUserInfo: (args, req) => queryDB(req, "select * from users where id = ?", [args.id]).then(data => data[0])
+  getUserInfo: (args, req) => queryDB(req, "select * from users where id = ?", [args.id]).then(data => data[0]),
+  updateUserInfo: (args, req) => queryDB(req, "update users SET ? where id = ?", [args, args.id]).then(data => data),
+  createUser: (args, req) => queryDB(req, "insert into users SET ?", args).then(data => data),
+  deleteUser: (args, req) => queryDB(req, "delete from users where id = ?", [args.id]).then(data => data)
 };
 
 // var root = {
 //   hello: () => "World"
 // };
+app.use((req, res, next) => {
+    
+    req.mysqlDb = mysql.createConnection({
+      host     : HOST  ,
+      user     : UNAME,
+      password : PASSWORD,
+      database : DATABASE
+    });
+  
+    req.mysqlDb.connect();
+
+    next();
+  }); 
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
-
-app.use((req, res, next) => {
-    
-  req.mysqlDb = mysql.createConnection({
-    host     : HOST  ,
-    user     : UNAME,
-    password : PASSWORD,
-    database : DATABASE
-  });
-
-  req.mysqlDb.connect();
-  next();
-}); 
 
 app.listen(PORT);
 
