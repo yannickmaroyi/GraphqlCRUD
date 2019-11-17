5a8efcc109f52d5c51afcc9f4b7b5042f7ee3767
yannickmaroyi <yannickmaroyi@gmail.com>
2019-11-17 11:05:16 +0300

    client changes and index adding imports

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        dont type such things
         </a>
       </header>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider} from '@apollo/react-hooks';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
